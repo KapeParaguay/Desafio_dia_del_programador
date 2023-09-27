e4ae412f60459b97b733aaa4d8a9e0c4b5f7817f
Fabrizio <122065986+KapeParaguay@users.noreply.github.com>
2023-09-27 18:28:42 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,18 +1,18 @@
 import unittest
-from desafio import dia_programador
+from desafio import dia_del_programador
 
 class TestDiaProgramador(unittest.TestCase):
 
     def test_dia_programador_2023(self):
         # Prueba para el año 2023
         year = 2023
-        resultado = dia_programador(year)
+        resultado = dia_del_programador(year)
         self.assertEqual(resultado, [13, 'September', 2023, 'Wednesday', 'celebrará'])
 
     def test_dia_programador_2022(self):
         # Prueba para el año 2022
         year = 2022
-        resultado = dia_programador(year)
+        resultado = dia_del_programador(year)
         self.assertEqual(resultado, [13, 'September', 2022, 'Tuesday', 'celebrará'])
 
 
